e4b116e53d3cb6d8b705ff788488f78431133fe6
Jani Eväkallio <jani.evakallio@gmail.com>
2016-02-28 18:04:36 +0000

    Default to complex examples

--- experiments/components/Home.js
+++ experiments/components/Home.js
@@ -1,14 +1,16 @@
 import {PropTypes} from 'react';
 import DerivableComponent from '../../lib/DerivableComponent';
 import html from '../util/html';
-import * as AppState from '../state/AppState';
 import Log from './Log';
 
+import * as AppState from '../state/AppState';
+
 const {div, h3, button} = html;
+
 const source = {
   count: {
     type: PropTypes.number.isRequired,
-    value: AppState.Count
+    value: AppState.Numbers.derive(ns => ns.size)
   },
   logVisible: {
     type: PropTypes.bool.isRequired,
@@ -18,14 +20,11 @@ const source = {
 
 const Home = DerivableComponent((probe, props = source) => probe ? props : (
   div({},
-    // update state
     button({onClick: AppState.addNumber}, 'Add log entry'),
     button({onClick: AppState.toggleLog},
       props.logVisible ? 'Hide log' : 'Show log'),
 
     h3({}, `Log entries: ${props.count}`),
-
-    // a nested derivable component
     props.logVisible && Log()
   )
 ));
